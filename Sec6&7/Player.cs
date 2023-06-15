5881de2fd01d02b397b03d7e90bce63f98f4cb9e
Łukasz Rak <lukarak818@student.polsl.pl>
2023-06-15 19:02:29 +0200

    json & using

---
+++ Sec6&7/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Player : IDisposable
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int HealthPoints { get; set; }
+        public List<Statistic> Statistics { get; set; }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Player disposed");
+        }
+    }
+}
--- Sec6&7/Program.cs
+++ Sec6&7/Program.cs
@@ -1,6 +1,6 @@
-﻿
-using System.Linq;
-using System.Text;
+﻿using program;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
 
 class Program
 {
@@ -30,25 +30,38 @@ class Program
 
         foreach (string word in words.Reverse()) 
         {
-            Console.WriteLine(word);
+        //    Console.WriteLine(word);
             reverseWords[i] = word;
             i++;
         }
         
        string finalResult = string.Join(' ', reverseWords);
-       Console.WriteLine(finalResult);
+        // Console.WriteLine(finalResult);
 
 
         //FILE HANDLING//////////////////////////////////////
-        ReadFiles();
-        GenerateDocuments();
-        ScanAndAppend();
+        // ReadFiles();
+        // GenerateDocuments();
+        // ScanAndAppend();
 
         //(DE)SERIALIZATION TO JSON
+        //Json();
 
+        //USING//////////////////////////////////////////////
+        using (var readFileStream = new FileStream(@"..\FILE-TEST\doc1.txt", FileMode.Open))
+        { 
+            //do smth with the file
+        
+        } //block using will close the file in a safe way (no memory leaks)
 
+        using (Player player = new Player())
+        {
+            //do whatever
+            //the class can be used only in this block
+        }
 
-
+        //var writeFileStream = new FileStream(@"..\FILE-TEST\doc1.txt", FileMode.Append);
+        //writeFileStream.Close(); this method is not safe because if an exception is thrown, the file will not be closed
     }
 
     static void ReadFiles()
@@ -86,6 +99,34 @@ class Program
         {
             File.AppendAllText(file, "\n All rights reserved");
         }
+    }
+
+    static void Json()
+    {
+        var player = new Player()
+        {
+            Name = "John",
+            Level = 1,
+            HealthPoints = 100,
+            Statistics = new List<Statistic>()
+            {
+                new Statistic() { Name = "Strength", Points = 10 },
+                new Statistic() { Name = "Agility", Points = 5 },
+                new Statistic() { Name = "Intelligence", Points = 15 }
+            }
+        };
+
+        player.Level++;
+
+        var jsonPlayer = JsonConvert .SerializeObject(player);
+        Console.WriteLine(jsonPlayer);
+
+        File.WriteAllText(@"..\FILE-TEST\player.json", jsonPlayer);
+
+        string readFile = File.ReadAllText(@"..\FILE-TEST\player.json");
+
+        Player readPlayer = JsonConvert.DeserializeObject<Player>(readFile);
+        Console.WriteLine(readPlayer.Name);
     }
 }
 
