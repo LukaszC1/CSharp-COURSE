7aa5a0fc8a6ecb3b8a70896339983334301016c9
Łukasz Rak <lukarak818@student.polsl.pl>
2023-06-20 15:43:48 +0200

    generic types

---
+++ GenericTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericTypes { 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var restaurants = new List<Restaurant>();
+
+            var paginatedResult = new PaginatedResult<Restaurant>
+            {
+                Results = restaurants,
+                ResultsFrom = 1,
+                ResultsTo = 10,
+                TotalPages = 1,
+                TotalResults = 10
+            };
+
+            var users = new List<User>();
+        }
+    }
+}
+
